9b09afed5a5651cf1b34fbdacbd9607c47723993
625673575 <625673575@qq.com>
2017-04-25 23:30:57 +0800

    添加mnist两种训练和预测使用方法

--- util/distribution.py
+++ util/distribution.py
@@ -8,8 +8,9 @@ FLAGS = None
 
 def main():
     # In task 0:
-    cluster = tf.train.ClusterSpec({"local": ["http://192.168.0.3:2223", "http://192.168.0.4:2223"]})
-    server = tf.train.Server(cluster, job_name="local", task_index=0)
+    cluster = tf.train.ClusterSpec({"ps": ["192.168.0.3:2222", "192.168.0.4:2223"]})
+    server = tf.train.Server(cluster, job_name="ps", task_index=0)
+    sess = tf.Session(server.target)
     with tf.device("/job:ps/task:0"):
         weights_1 = tf.Variable(tf.constant( [1,2,3,4],tf.float32))
         biases_1 = tf.Variable(tf.constant( [1,2,3,4],tf.float32))
@@ -17,10 +18,10 @@ def main():
     with tf.device("/job:ps/task:1"):
         weights_2 = tf.Variable(tf.constant( [5,6,7,8],tf.float32))
         biases_2 = tf.Variable(tf.constant( [5,6,7,8],tf.float32))
+        sess.run(tf.global_variables_initializer())
         layer_1 = tf.multiply(biases_1, weights_1)
         logits = tf.multiply(biases_2, weights_2)
 
-    sess = tf.Session(server.target)
     print(sess.run(logits))
 
 main()
