991008a8ce4bdb01f0c4b55f36ff6990b59b52b2
625673575 <625673575@qq.com>
2017-04-25 23:24:06 +0800

    添加mnist两种训练和预测使用方法

--- util/distribution.py
+++ util/distribution.py
@@ -8,7 +8,7 @@ FLAGS = None
 
 def main():
     # In task 0:
-    cluster = tf.train.ClusterSpec({"local": ["192.168.0.3", "192.168.0.4"]})
+    cluster = tf.train.ClusterSpec({"local": ["http://192.168.0.3:2223", "http://192.168.0.4:2223"]})
     server = tf.train.Server(cluster, job_name="local", task_index=0)
     with tf.device("/job:ps/task:0"):
         weights_1 = tf.Variable(tf.constant( [1,2,3,4],tf.float32))
