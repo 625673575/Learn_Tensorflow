98e4ad367a98db89fe4694d1d4e002ade6eb8122
625673575 <625673575@qq.com>
2017-04-25 23:22:14 +0800

    添加mnist两种训练和预测使用方法

--- util/distribution.py
+++ util/distribution.py
@@ -6,72 +6,21 @@ import tensorflow as tf
 
 FLAGS = None
 
-def main(_):
-    ps_hosts = FLAGS.ps_hosts.split(",")
-    worker_hosts = FLAGS.worker_hosts.split(",")
+def main():
+    # In task 0:
+    cluster = tf.train.ClusterSpec({"local": ["192.168.0.3", "192.168.0.4"]})
+    server = tf.train.Server(cluster, job_name="local", task_index=0)
+    with tf.device("/job:ps/task:0"):
+        weights_1 = tf.Variable(tf.constant( [1,2,3,4],tf.float32))
+        biases_1 = tf.Variable(tf.constant( [1,2,3,4],tf.float32))
 
-    cluster = tf.train.ClusterSpec({"ps": ps_hosts, "worker": worker_hosts})
+    with tf.device("/job:ps/task:1"):
+        weights_2 = tf.Variable(tf.constant( [5,6,7,8],tf.float32))
+        biases_2 = tf.Variable(tf.constant( [5,6,7,8],tf.float32))
+        layer_1 = tf.multiply(biases_1, weights_1)
+        logits = tf.multiply(biases_2, weights_2)
 
-    # Create and start a server for the local task.
-    server = tf.train.Server(cluster,job_name=FLAGS.job_name,task_index=FLAGS.task_index)
+    sess = tf.Session(server.target)
+    print(sess.run(logits))
 
-    if FLAGS.job_name == "ps":
-        server.join()
-    elif FLAGS.job_name == "worker": # Assigns ops to the local worker by default.
-        with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d" % FLAGS.task_index,cluster=cluster)):
-
-      # Build model...
-      loss = ...
-      global_step = tf.contrib.framework.get_or_create_global_step()
-
-      train_op = tf.train.AdagradOptimizer(0.01).minimize(
-          loss, global_step=global_step)
-
-    # The StopAtStepHook handles stopping after running given steps.
-    hooks=[tf.train.StopAtStepHook(last_step=1000000)]
-
-    # The MonitoredTrainingSession takes care of session initialization,
-    # restoring from a checkpoint, saving to a checkpoint, and closing when done
-    # or an error occurs.
-    with tf.train.MonitoredTrainingSession(master=server.target,
-                                           is_chief=(FLAGS.task_index == 0),
-                                           checkpoint_dir="/tmp/train_logs",
-                                           hooks=hooks) as mon_sess:
-      while not mon_sess.should_stop():
-        # Run a training step asynchronously.
-        # See `tf.train.SyncReplicasOptimizer` for additional details on how to
-        # perform *synchronous* training.
-        # mon_sess.run handles AbortedError in case of preempted PS.
-        mon_sess.run(train_op)
-
-if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.register("type", "bool", lambda v: v.lower() == "true")
-  # Flags for defining the tf.train.ClusterSpec
-  parser.add_argument(
-      "--ps_hosts",
-      type=str,
-      default="",
-      help="Comma-separated list of hostname:port pairs"
-  )
-  parser.add_argument(
-      "--worker_hosts",
-      type=str,
-      default="",
-      help="Comma-separated list of hostname:port pairs"
-  )
-  parser.add_argument(
-      "--job_name",
-      type=str,
-      default="",
-      help="One of 'ps', 'worker'"
-  )
-  # Flags for defining the tf.train.Server
-  parser.add_argument(
-      "--task_index",
-      type=int,
-      default=0,
-      help="Index of task within the job"
-  )
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+main()
